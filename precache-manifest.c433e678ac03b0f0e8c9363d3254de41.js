06c85ad5de077fed458816d00d7e402c82f0bc4f
Arif <developebyarif@gmail.com>
2021-11-04 00:35:08 +0530

    Updates

---
+++ precache-manifest.c433e678ac03b0f0e8c9363d3254de41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38c274a5fd62ce1311604843a8f2adb1",
+    "url": "/mfe-micro-app-1/index.html"
+  },
+  {
+    "revision": "2348f5f6cd4a26b3c779",
+    "url": "/mfe-micro-app-1/static/css/main.d1b05096.css"
+  },
+  {
+    "revision": "2348f5f6cd4a26b3c779",
+    "url": "/mfe-micro-app-1/static/js/main.93e22146.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/mfe-micro-app-1/static/js/main.93e22146.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mfe-micro-app-1/static/media/logo.5d5d9eef.svg"
+  }
+]);
