12e6a7856d1bd53f89748060fbb2f346de78aa78
Arif <developebyarif@gmail.com>
2021-11-04 01:06:44 +0530

    Updates

--- precache-manifest.c069d0bf583f554ae28a90932c098e8d.js
+++ precache-manifest.c069d0bf583f554ae28a90932c098e8d.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "141883c598996b01d9bbaf93b878ce14",
+    "revision": "a9c0490621746e9e71fade4a0fb8864d",
     "url": "/index.html"
   },
   {
-    "revision": "6e3db1e171c2280b40db",
+    "revision": "fc02f9190f1b7a14d0fb",
     "url": "/static/css/main.d1b05096.css"
   },
   {
-    "revision": "6e3db1e171c2280b40db",
-    "url": "/static/js/main.a29ced43.js"
+    "revision": "fc02f9190f1b7a14d0fb",
+    "url": "/static/js/main.d0f81bc8.js"
   },
   {
     "revision": "570d362d673dab785e62d2b8563e1118",
-    "url": "/static/js/main.a29ced43.js.LICENSE.txt"
+    "url": "/static/js/main.d0f81bc8.js.LICENSE.txt"
   },
   {
     "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
