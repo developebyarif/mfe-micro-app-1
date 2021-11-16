20a707c840083c103c40298a999f21498944cb63
Arif <developebyarif@gmail.com>
2021-11-16 14:34:27 +0530

    Updates

---
+++ precache-manifest.6ec7665b39880f206761e06a3919446e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb48f47c334a16ccafe8414b10ed2c89",
+    "url": "https://developebyarif.github.io/mfe-micro-app-1/index.html"
+  },
+  {
+    "revision": "4668be6c0619e1093b74",
+    "url": "https://developebyarif.github.io/mfe-micro-app-1/static/css/main.062c6aa4.css"
+  },
+  {
+    "revision": "4668be6c0619e1093b74",
+    "url": "https://developebyarif.github.io/mfe-micro-app-1/static/js/main.62de7e0b.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "https://developebyarif.github.io/mfe-micro-app-1/static/js/main.62de7e0b.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "https://developebyarif.github.io/mfe-micro-app-1/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://developebyarif.github.io/mfe-micro-app-1/precache-manifest.59df28d7d5f941c09a3baf02548a17ae.js"
+  "https://developebyarif.github.io/mfe-micro-app-1/precache-manifest.6ec7665b39880f206761e06a3919446e.js"
 );
 
 self.addEventListener('message', (event) => {
